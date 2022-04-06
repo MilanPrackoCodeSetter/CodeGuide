bb1e2c35407ab488769346d8c0d63b877fc4a3c5
Milan Pracko <milan.pracko@valatron.com>
2022-04-06 14:35:16 +0200

    printer

---
+++ CodeGuideTools/CodeGuidePrinter/Program.cs
@@ -0,0 +1,17 @@
+﻿
+var pathToFiles = Path.Combine(Environment.CurrentDirectory, @"..\..\..\..\..\");
+var directories = Directory.GetDirectories(pathToFiles);
+
+foreach (var directory in directories)
+{
+    var files = Directory.GetFiles(directory);
+    foreach (var file in files)
+    {
+        Console.WriteLine(directory.Replace(pathToFiles, string.Empty));
+        var content = File.ReadAllText(file);
+        Console.WriteLine(content);
+        Console.WriteLine();
+    }
+}
+
+Console.ReadKey();
